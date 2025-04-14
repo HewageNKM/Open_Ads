eea63bf33cbef3d486f251dcbea52e00ebe2a174
Ethan Blake <107237482+EthanBlake00@users.noreply.github.com>
2025-04-15 00:31:33 +0530

    add db_config

--- db/db_config.php
+++ db/db_config.php
@@ -1,5 +1,20 @@
 <?php
 // Create the table before hand
-session_start()
+
+session_start();
+
+$username = "root";
+$passowrd = "";
+$host = "localhost";
+$db_name = "open_ads";
+
+$conn = new mysqli($host, $username, $passowrd, $db_name);
+
+if ($conn->connect_error) {
+    $_SESSION["db_connection"] = "Failed to connect to database";
+    die("Connection failed: " . $conn->connect_error);
+}else{
+    $_SESSION["db_connection"] = "Connected to database";
+}
 
 ?>
